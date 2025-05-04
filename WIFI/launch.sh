5f0bb8bf679a0982f8fcbc3d2395896c6164fd7c
Josh Shippam <darkaidz98@gmail.com>
2025-05-04 01:07:29 +0100

    Adapt For Miyoo Flip Stock OS

--- WIFI/launch.sh
+++ WIFI/launch.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+if [ ! -f /data/cfg/wpa_supplicant.wifi ]; then
+    cp wpa_supplicant.wifi /data/cfg/
+fi
+
 echo $0 $*
 
-tail -4 /appconfigs/wpa_supplicant.conf >> /appconfigs/wpa_supplicant.cunt
-cp -f /appconfigs/wpa_supplicant.cunt /appconfigs/wpa_supplicant.conf
+echo "" >> /data/cfg/wpa_supplicant.wifi
+tail -4 /data/cfg/wpa_supplicant.conf >> /data/cfg/wpa_supplicant.wifi
+cp -f /data/cfg/wpa_supplicant.wifi /data/cfg/wpa_supplicant.conf
